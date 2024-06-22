9b0316424b46d9c7d0fd27033831ef08fa5f06ba
NguyenThanhTin <asboperman2507@gmail.com>
2024-06-22 09:13:10 +0700

    Temp1.3 - Giao diện Suatrithuc

---
+++ projectLoXo/Pages/Suatrithuc.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace projectLoXo.Pages
+{
+    public class SuatrithucModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
